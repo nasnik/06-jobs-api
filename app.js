e28a09e1d79f8a68b99fe217a84c8e514bdbeada
nasnik <nikoulkina@gmail.com>
2025-01-22 21:04:35 -0800

    week 10 homework added api docs through swagger

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const cors = require('cors');
 const xss = require('xss-clean');
 const rateLimiter = require('express-rate-limit');
 
+//Swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 const express = require('express');
 const app = express();
 
@@ -34,8 +39,9 @@ app.use(cors());
 app.use(xss());
 
 app.get('/', (req, res) => {
-  res.send('Activities API')
+  res.send('<h1>Activities API</h1><a href="/api-docs">Documentation</a>')
 })
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 // routes
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/activities', authenticateUser, activitiesRouter);
